6ebfa9e9e2ffe62b988a6bbeb5bdc9fa777444b4
Sacha Coppey <sacha.coppey@oracle.com>
2022-08-04 16:24:04 +0200

    Add imports

--- src/hotspot/cpu/riscv/jvmciCodeInstaller_riscv.cpp
+++ src/hotspot/cpu/riscv/jvmciCodeInstaller_riscv.cpp
@@ -22,12 +22,16 @@
  *
  */
 
+#include "precompiled.hpp"
+#include "asm/macroAssembler.hpp"
+#include "jvmci/jvmci.hpp"
 #include "jvmci/jvmciCodeInstaller.hpp"
 #include "jvmci/jvmciRuntime.hpp"
 #include "jvmci/jvmciCompilerToVM.hpp"
 #include "jvmci/jvmciJavaClasses.hpp"
 #include "oops/oop.inline.hpp"
 #include "runtime/handles.inline.hpp"
+#include "runtime/jniHandles.hpp"
 #include "runtime/sharedRuntime.hpp"
 #include "vmreg_riscv.inline.hpp"
 
