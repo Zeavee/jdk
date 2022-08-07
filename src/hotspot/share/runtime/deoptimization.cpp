17f7767f92808fbc1e8707d7fdcc9714e50917e1
Sacha Coppey <sacha.coppey@oracle.com>
2022-08-07 13:55:09 +0200

    Added print

--- src/hotspot/share/runtime/deoptimization.cpp
+++ src/hotspot/share/runtime/deoptimization.cpp
@@ -1912,6 +1912,7 @@ JRT_ENTRY(void, Deoptimization::uncommon_trap_inner(JavaThread* current, jint tr
     int debug_id = trap_request_debug_id(trap_request);
 #endif
     jint unloaded_class_index = trap_request_index(trap_request); // CP idx or -1
+    fprintf(stderr, "This is unloaded_class_index %d\n", unloaded_class_index);
 
     vframe*  vf  = vframe::new_vframe(&fr, &reg_map, current);
     compiledVFrame* cvf = compiledVFrame::cast(vf);
